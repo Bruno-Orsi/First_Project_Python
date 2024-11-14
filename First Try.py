6b61b9a50fb1a28eee1a22e5d2c36a062c201ba5
Bruno-Orsi <bruno_orsi@hotmail.com>
2024-11-14 11:03:44 -0300

    Add files via upload

---
+++ First Try.py
@@ -0,0 +1,96 @@
+#print("qual seu nome")
+#nome = input()
+nome = input("Qual seu nome")
+
+
+# print('Seja bem-vindo(a) %s' % nome)
+# print('Seja bem-vindo(a) {0}'.format(nome))
+print(f"Seja bem-vindo(a) {nome}?")
+
+#print('Qual sua idade?')
+#idade = input()
+idade = input("qual sua idade?")
+
+
+
+# print("A %s tem %s anos" % (nome, idade))
+# print('A {0} tem {1} anos'.format(nome, idade))
+print(f'O/A {nome} tem {idade}')
+
+print(f"O/A {nome} nasceu em {2024 - int(idade)}")
+
+
+#print("A %s, quer casar comigo?" % (nome))
+#print("A {0}, quer casar comigo?".format(nome))
+print(f'O/A {nome}, gostou do programa?')
+
+
+#print("sim ou não?")
+#sim_ou_não = input()
+sim_ou_nao = input("sim ou não?")
+
+
+input("Obrigado por participar!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
